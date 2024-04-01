773fb3e21cad89dc5c9700c6efd677eb48ef2142
Shashidhar Angadi <shashidharangadi14@gmail.com>
2024-04-01 21:06:01 +0530

    Axios implementation

--- src/App.js
+++ src/App.js
@@ -9,40 +9,39 @@ import Missing from './Missing';
 import { Route, Switch, useHistory } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 import { format } from 'date-fns';
+import api from './api/posts';
+import EditPost from './EditPost';
 
 function App() {
-    const [posts,setPosts] = useState([
-      {
-        id: 1,
-        title: "My First Post",
-        datetime: "July 01, 2021 11:17:36 AM",
-        body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis consequatur expedita, assumenda similique non optio! Modi nesciunt excepturi corrupti atque blanditiis quo nobis, non optio quae possimus illum exercitationem ipsa!"
-      },
-      {
-        id: 2,
-        title: "My 2nd Post",
-        datetime: "July 01, 2021 11:17:36 AM",
-        body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis consequatur expedita, assumenda similique non optio! Modi nesciunt excepturi corrupti atque blanditiis quo nobis, non optio quae possimus illum exercitationem ipsa!"
-      },
-      {
-        id: 3,
-        title: "My 3rd Post",
-        datetime: "July 01, 2021 11:17:36 AM",
-        body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis consequatur expedita, assumenda similique non optio! Modi nesciunt excepturi corrupti atque blanditiis quo nobis, non optio quae possimus illum exercitationem ipsa!"
-      },
-      {
-        id: 4,
-        title: "My Fourth Post",
-        datetime: "July 01, 2021 11:17:36 AM",
-        body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis consequatur expedita, assumenda similique non optio! Modi nesciunt excepturi corrupti atque blanditiis quo nobis, non optio quae possimus illum exercitationem ipsa!"
-      }
-    ]);
+    const [posts,setPosts] = useState([]);
 
     const [search,setSearch]=useState('');
     const [searchResults,setSearchResults] = useState([]);
     const [postTitle,setPostTitle] = useState('');
     const [postBody,setPostBody] = useState('');
+    const [editTitle,setEditTitle] = useState('');
+    const [editBody,setEditBody] = useState('');
     const history = useHistory();
+
+    useEffect(()=>{
+      const fetchPosts = async ()=>{
+        try{
+          const response = await api.get('/posts');
+          setPosts(response.data);
+        }catch(err){
+          if(err.response){
+            //Not in the 200 response range(but we got response instance from the backend)
+            console.log(err.response.data);
+            console.log(err.response.status);
+            console.log(err.response.headers);
+          }else{
+            //if incase of any page not found error 
+            console.log(`Error: ${err.message}`);
+          } 
+        }
+      }
+      fetchPosts();
+    },[]);
 
     useEffect(()=>{
       const filteredResults = posts.filter(post =>//This one is little awkward that, we cannot put {} it will not get you the output
@@ -52,22 +51,47 @@ function App() {
       setSearchResults(filteredResults.reverse());
     },[posts, search])
 
-    const handleSubmit = (e)=>{
+    const handleSubmit = async(e)=>{
       e.preventDefault();
-      const id = posts.length ? posts[posts.length - 1].id + 1: 1;
+      const id = posts.length ? String(Number(posts[posts.length - 1].id) + 1): "1";
       const datetime = format(new Date(), 'MMMM dd, yyyy pp')//Look for the library in the npmjs website 
       const newPost = { id, title: postTitle, datetime, body: postBody };
-      const allPosts = [...posts,newPost];
-      setPosts(allPosts);
-      setPostTitle('');
-      setPostBody('');
-      history.push('/');
+      try{
+        const response = await api.post('/posts', newPost);
+        const allPosts = [...posts,response.data];
+        setPosts(allPosts);
+        setPostTitle('');
+        setPostBody('');
+        history.push('/');
+      }catch(err){
+        console.log(`Error: ${err.message}`);//We can further extend this as mentioned above in useEffect of fetching the data(error part)
+      }
     }
 
-    const handleDelete = (id)=>{
-      const postsList = posts.filter(post => post.id!==id);
-      setPosts(postsList);
-      history.push('/');//User in the page that is been called for deletion hence to move from that page we use the hook useHistroy from react
+    const handleEdit = async(id)=>{
+      const datetime = format(new Date(), 'MMMM dd, yyyy pp')
+      const updatedPost = { id, title: editTitle, datetime, body: editBody };
+      try{
+        const response = await api.put(`/posts/${id}`,updatedPost);//Now we use put.. we could use patch if we are updating specific fields ..Here we are essentially be replacing the entire post
+        setPosts(posts.map(post => (post.id).toString() === id ? response.data: post));
+        setEditTitle('');
+        setEditBody('');
+        history.push('/');
+      }catch(err){
+        console.log(`Error: ${err.message}`);//We can further extend this as mentioned above in useEffect of fetching the data(error part)
+      }
+
+    }
+
+    const handleDelete = async(id)=>{
+      try{
+        await api.delete(`/posts/${id}`);
+        const postsList = posts.filter(post => post.id!==id);
+        setPosts(postsList);
+        history.push('/');//User in the page that is been called for deletion hence to move from that page we use the hook useHistroy from react
+      }catch(err){
+        console.log(`Error: ${err.message}`);
+      }
     }
 
   return (
@@ -81,6 +105,9 @@ function App() {
       </Route>
       <Route exact path="/post">
         <NewPost handleSubmit={handleSubmit} postTitle={postTitle} setPostTitle={setPostTitle} postBody={postBody} setPostBody={setPostBody}/>
+      </Route>
+      <Route exact path="/edit/:id">
+        <EditPost posts={posts} handleEdit={handleEdit} editTitle={editTitle} setEditTitle={setEditTitle} editBody={editBody} setEditBody={setEditBody}/>
       </Route>
       <Route path="/post/:id">
         <PostPage posts={posts} handleDelete={handleDelete}/>
